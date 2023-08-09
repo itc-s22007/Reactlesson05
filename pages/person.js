832ab2d62db58db567e69e702fc4e5ddeaa8cca5
itc-s22007 <s22007@std.it-college.ac.jp>
2023-08-09 10:51:45 +0000

    2page start

--- pages/person.js
+++ pages/person.js
@@ -1,7 +1,11 @@
+import styles from "../styles/person.module.css"
 export default function Person() {
   return(
-    <div>
-      <h1>会話したい人物名</h1>
+    <div className={styles.container}>
+       <h1>会話したい人物名</h1>
+	  <button className={styles.button1}>上司</button>
+	  <button className={styles.button2}>恋人</button>
+	  <button className={styles.button3}>お客様</button>
     </div>
   )
 }
