e0d8781a26b54472ee78fd55602b9f69ae164d05
s22013 <s22013@std.it-college.ac.jp>
2023-08-14 02:58:11 +0000

    円グラフと文字配置を作成

--- pages/result.js
+++ pages/result.js
@@ -1,11 +1,15 @@
-import styles from '../styles/result.module.css'
+import styles from '../styles/result.module.css';
+
 export default function Person() {
   return(
     <div className={styles.container}>
-      <h1>会話したい人物名</h1>
-      <div className={styles.circle}></div>
-	<p>親密度</p>
-	<h1>100%</h1>
+      <h1 className={styles.box01}>会話したい人物名</h1>
+      <div className={styles.circle}>
+	<div className={styles.intimacyContainer}>  
+	<p className={styles.intimacy}>親密度</p>
+	<h1 className={styles.intimacyValue}>100%</h1>
+	</div>  
+      </div>  
     </div>
   );
 }       
