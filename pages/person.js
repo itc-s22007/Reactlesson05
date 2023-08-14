e7ccac5f70267306d264e90199fcf21f8c80a55b
itc-s22007 <s22007@std.it-college.ac.jp>
2023-08-14 01:00:28 +0000

    一通り終わり

--- pages/person.js
+++ pages/person.js
@@ -1,4 +1,5 @@
-import styles from "../styles/person.module.css"
+import Link from 'next/link';
+import styles from '../styles/person.module.css'
 export default function Person() {
   return(
     <div className={styles.container}>
@@ -6,6 +7,12 @@ export default function Person() {
 	  <button className={styles.button1}>上司</button>
 	  <button className={styles.button2}>恋人</button>
 	  <button className={styles.button3}>お客様</button>
+	  <div className={styles.content}>
+	    <Link href="/">
+	      <button className={styles.button4}>戻る</button>
+	    </Link>
+	    <button className={styles.button5}>決定</button>
+	  </div>
     </div>
   )
 }
